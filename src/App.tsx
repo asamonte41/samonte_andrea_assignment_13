cc7cf53d89bb201572c44c45511b17bf27589a36
asamonte41 <asamonte41@academic.rrc.ca>
2025-10-11 23:39:50 -0500

    App edit and added index in components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Button from "./components/Button";
+import { Button } from './components';
+import { Label } from './components';
+import { Text } from './components';
 
 function App() {
   return (
     <div className="App">
-      <button/>
+      <Button label="Submit" onClick={() => alert('Form submitted!')} />
+
+     <Label text="Username" htmlFor="username" required />
+      <input id="username" type="text" placeholder="Enter your username" />
+
+    <Text size="md" color="muted" align="left">
+        Please choose a unique username. It must be at least 6 characters.
+      </Text>
+
     </div>
   );
 }
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export * from './Button';
+export * from './Label';
+export * from './Text';
+// Add more as you scaffold new components
